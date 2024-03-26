a934b7e1cc64b68f7ca726a70210bb7e7536b4be
Muhammad Hassan Bilal <hassansheikh9p9@gmail.com>
2024-03-26 16:11:22 +0500

    Made changes to index.html and library.js

--- library.js
+++ library.js
@@ -9,35 +9,59 @@ document.addEventListener('DOMContentLoaded', function () {
     const clear = document.getElementById('clear-btn');
     const bookList = document.getElementById('book-list');
     const modal = document.getElementById('editmodal');
-    let book = {};
-    let books = [];
+    let editId = null;
+    let editElement;
 
+    window.addEventListener('DOMContentLoaded', loadItems);
     addBtn.addEventListener('click', addItem);
+    clear.addEventListener('click', delAll);
 
     function addItem(e) {
         e.preventDefault();
         if (name.value === '' || author.value === '' || genre.value === '' || year.value === '' || quantity.value === '') {
             alert('Please fill in all fields');
         } else {
-            book = {
+            const id = new Date().getTime().toString();
+            let book = {
                 name: name.value,
                 author: author.value,
                 genre: genre.value,
                 year: year.value,
                 quantity: quantity.value
             };
-            createItem(book);
-            saveLocally(book);
+            createItem(id, book);
+            saveLocally(id, book);
         }
     }
 
-    function saveLocally(_book) {
-        books.push(_book);
-        localStorage.setItem('books', JSON.stringify(books));
+    function delAll() {
+        localStorage.removeItem('books');
+        bookList.innerHTML = '';
+        reset();
     }
 
-    function createItem(book) {
+    function saveLocally(id, _book) {
+        let item = {
+            id,
+            book: _book
+        }
+        let blist = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+        blist.push(item);
+        localStorage.setItem('books', JSON.stringify(blist));
+    }
+
+    function loadItems() {
+        let blist = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+        if (blist.length > 0) {
+            blist.forEach(item => {
+                createItem(item.id, item.book);
+            });
+        }
+    }
+
+    function createItem(id, book) {
         const row = document.createElement('tr');
+        row.setAttribute('data-id', id);
         row.innerHTML = `<td>${book.name}</td>
         <td>${book.author}</td>
         <td>${book.genre}</td>
@@ -49,17 +73,37 @@ document.addEventListener('DOMContentLoaded', function () {
             <button id="delete-btn" class="btn btn-danger">Delete</button>
         </td>`
         bookList.appendChild(row);
-        // document.getElementById('delete-btn').addEventListener('click', deleteItem);
-        document.getElementById('edit-btn').addEventListener('click', editItem);
+        row.querySelector('#delete-btn').addEventListener('click', deleteItem);
+        row.querySelector('#edit-btn').addEventListener('click', editItem);
         reset();
     }
 
-    function editItem() {
-        modal.getElementById('name').value = book.name;
-        modal.getElementById('author').value = book.author;
-        modal.getElementById('genre').value = book.genre;
-        modal.getElementById('year').value = book.year;
-        modal.getElementById('quantity').value = book.quantity;
+    function deleteItem(e) {
+        if (confirm('Are you sure you want to delete this item?')) {
+            const element = e.currentTarget.parentElement.parentElement;
+            const id = element.dataset.id;
+            bookList.removeChild(element);
+            removeLocally(id);
+            reset();
+        }
+    }
+
+    function removeLocally(id) {
+        let blist = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+        blist = blist.filter(item => item.id !== id);
+        localStorage.setItem('books', JSON.stringify(blist));
+        if (blist.length === 0)
+            localStorage.clear();
+    }
+
+    function editItem(e) {
+        editElement = e.currentTarget.parentElement.parentElement;
+        editId = editElement.dataset.id;
+        document.getElementsByClassName('name')[0].value = editElement.children[0].textContent;
+        document.getElementsByClassName('author')[0].value = editElement.children[1].textContent;
+        document.getElementsByClassName('genre')[0].value = editElement.children[2].textContent;
+        document.getElementsByClassName('year')[0].value = editElement.children[3].textContent;
+        document.getElementsByClassName('quantity')[0].value = editElement.children[4].textContent;
     }
 
     function reset() {
@@ -68,6 +112,5 @@ document.addEventListener('DOMContentLoaded', function () {
         genre.value = '';
         year.value = '';
         quantity.value = '';
-        book = {};
     }
 });
